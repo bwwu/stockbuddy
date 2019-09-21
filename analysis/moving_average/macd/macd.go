a456d967a573b36bfd2a42c27359500700ab9902
Brandon Wu <brandonwu23@gmail.com>
2019-09-21 13:44:23 -0700

    feat(macd): Implement MACD(12,26,9)

---
+++ analysis/moving_average/macd/macd.go
@@ -0,0 +1,21 @@
+package macd
+
+import (
+  "stockbuddy/analysis/moving_average/sma/sma"
+)
+
+// MovingAverageConvergenceDivergenceSeries calculates the difference between
+// 12-Period and 26-Period EMA. Generally compared against a Signal line 
+// generated from a 9-Day EMA of the MACD
+func MovingAverageConvergenceDivergenceSeries(prices []float64) []float64 {
+  twelvePeriodEMA := sma.ExponentialMovingAverageSeries(12, prices)
+  twentySixPeriodEMA := sma.ExponentialMovingAverageSeries(26, prices)
+
+  macd := make([]float64, len(twentySixPeriodEMA))
+  twelvePeriodEMA = twelvePeriodEMA[len(twelvePeriodEMA)-len(macd):]
+
+  for i:= 0; i<len(macd); i++ {
+    macd[i] = twelvePeriodEMA[i] - twentySixPeriodEMA[i]
+  }
+  return macd
+}
