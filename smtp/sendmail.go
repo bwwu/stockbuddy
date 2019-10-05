16db11c17f310e5237a6617868e9baf1e57cee89
brandon <brandonwu23@gmail.com>
2019-10-05 13:50:48 -0700

    chore(smtp): reformat sendmail

--- smtp/sendmail.go
+++ smtp/sendmail.go
@@ -1,24 +1,24 @@
 package sendmail
 
 import (
-	"fmt"
-	"log"
-	"strings"
-	"net/smtp"
+  "fmt"
+  "log"
+  "strings"
+  "net/smtp"
 )
 
 type Email struct {
-	Body, Subject string
-	Recipients []string
+  Body, Subject string
+  Recipients []string
 }
 
 func (e *Email) Send() error {
-	auth := smtp.PlainAuth(defaultSender.name, defaultSender.email, defaultSender.password, smtpServer)
-	if err := smtp.SendMail(smtpServer + ":587", auth, defaultSender.email, e.Recipients, getEmailContent(e)); err != nil {
-    log.Println(err.Error())
+  auth := smtp.PlainAuth(defaultSender.name, defaultSender.email, defaultSender.password, smtpServer)
+  if err := smtp.SendMail(smtpServer + ":587", auth, defaultSender.email, e.Recipients, getEmailContent(e)); err != nil {
+    log.Print(err)
     return err
-	}
-	log.Printf("Email(s) sent to %s", strings.Join(e.Recipients, ","))
+  }
+  log.Printf("Email(s) sent to %s", strings.Join(e.Recipients, ","))
   return nil
 }
 
@@ -26,17 +26,17 @@ var smtpServer = "smtp.gmail.com"
 var defaultSender = sender{"tradingbot88@gmail.com", "Trade Bot", "makemoney888$"}
 
 type sender struct {
-	email, name, password string
+  email, name, password string
 }
 
 
 func getEmailContent(e *Email) []byte {
-	content := fmt.Sprintf(
+  content := fmt.Sprintf(
     "From:%s\nTo:%s\nSubject:%s\nContent-Type:text/html;charset=\"utf-8\"\n<html>\n%s\n</html>",
     defaultSender.name,
     strings.Join(e.Recipients, ","),
     e.Subject,
     e.Body,
   )
-	return []byte(content)
+  return []byte(content)
 }
