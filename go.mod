0458261a859e2b1b6466e129f07c4a052832c8bf
Brandon Wu <4888912+bwwu@users.noreply.github.com>
2023-04-17 21:30:58 -0700

    Fix yahoo finance timeouts (#18)
    * Add ResponseHeaderTimeout to http calls
    
    * Fix cookie and print status
    
    * Impl worker pool to limit concurrent http requests
    
    ---------
    
    Co-authored-by: Brandon Wu <bwdw@google.com>

--- go.mod
+++ go.mod
@@ -1,3 +1,5 @@
 module github.com/bwwu/stockbuddy
 
 go 1.20
+
+require golang.org/x/sync v0.1.0 // indirect
--- quote/yahoofinance/yahoo_finance_client.go
+++ quote/yahoofinance/yahoo_finance_client.go
@@ -7,10 +7,12 @@ import (
 	"net/http"
 	"strconv"
 	"time"
+	"golang.org/x/sync/semaphore"
 )
 
 type YahooFinanceClient struct {
 	client http.Client
+	workerPool *semaphore.Weighted
 }
 
 type YFQuote struct {
@@ -19,42 +21,49 @@ type YFQuote struct {
 	Volume                           uint64
 }
 
-func NewYahooFinanceClient(timeoutInSec int) *YahooFinanceClient {
+func NewYahooFinanceClient(timeoutInSec, maxWorkers int) *YahooFinanceClient {
 	return &YahooFinanceClient{
 		client: http.Client{
-			Timeout: time.Duration(timeoutInSec) * time.Second,
+			Timeout: time.Duration(timeoutInSec)*time.Second,
+			Transport: &http.Transport{
+				ResponseHeaderTimeout: time.Duration(timeoutInSec)*time.Second,
+			},
 		},
+		workerPool: semaphore.NewWeighted(int64(maxWorkers)),
 	}
 }
 
 func (y *YahooFinanceClient) GetQuoteHistory(ctx context.Context, symbol string, days int) ([]*YFQuote, error) {
-	cookies := map[string]string{
-		"B":      "ajch0f5elj4sp",
-		"APID":   "1Adf2ce59c-c1e2-11e9-adce-025f25c4bfdc",
-		"APIDTS": "1566870258",
-		"PRF":    "t%3D" + symbol,
+	if err := y.workerPool.Acquire(ctx, 1); err != nil {
+		return nil, fmt.Errorf(
+			"yahoo_finance_client::GetQuoteHistory: failed to aquire semaphore: %v",
+			err,
+		)
 	}
+	defer y.workerPool.Release(1)
+
 	timeEnd := time.Now()
 	timeStart := timeEnd.AddDate(0, 0, -1*days)
 
 	// Params: symbol, start timestamp, end timestamp
 	const urlFormat = "https://query1.finance.yahoo.com/v7/finance/download/%s?period1=%d&period2=%d&interval=1d&events=history&crumb=pRB6UiIiFnn"
-
-	req, err := http.NewRequestWithContext(ctx, "GET", fmt.Sprintf(urlFormat, symbol, timeStart.Unix(), timeEnd.Unix()), nil)
+	reqUrl := fmt.Sprintf(urlFormat, symbol, timeStart.Unix(), timeEnd.Unix())
+	req, err := http.NewRequestWithContext(ctx, "GET", reqUrl, nil)
 	if err != nil {
 		return nil, err
-	}
-
-	for key, val := range cookies {
-		req.AddCookie(&http.Cookie{
-			Name:  key,
-			Value: val,
-		})
 	}
 
 	resp, err := y.client.Do(req)
 	if err != nil {
 		return nil, err
+	}
+
+	if resp.Status != "200 OK" {
+		return nil, fmt.Errorf(
+			`yahoo_finance_client::GetQuoteHistory: requested failed with status=%v, url="%v"`,
+			resp.Status,
+			reqUrl,
+		)
 	}
 
 	defer resp.Body.Close()
