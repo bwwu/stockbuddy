7c562a09b3aa5c77a6d9af4432a080c93fecc286
Brandon Wu <bwdw@google.com>
2023-04-17 21:10:30 -0700

    Impl worker pool to limit concurrent http requests

--- quote/yahoofinance/yahoo_finance_client.go
+++ quote/yahoofinance/yahoo_finance_client.go
@@ -7,10 +7,12 @@ import (
 	"net/http"
 	"strconv"
 	"time"
+	"golang.org/x/sync/semaphore"
 )
 
 type YahooFinanceClient struct {
 	client http.Client
+	workerPool *semaphore.Weighted
 }
 
 type YFQuote struct {
@@ -19,7 +21,7 @@ type YFQuote struct {
 	Volume                           uint64
 }
 
-func NewYahooFinanceClient(timeoutInSec int) *YahooFinanceClient {
+func NewYahooFinanceClient(timeoutInSec, maxWorkers int) *YahooFinanceClient {
 	return &YahooFinanceClient{
 		client: http.Client{
 			Timeout: time.Duration(timeoutInSec)*time.Second,
@@ -27,37 +29,42 @@ func NewYahooFinanceClient(timeoutInSec int) *YahooFinanceClient {
 				ResponseHeaderTimeout: time.Duration(timeoutInSec)*time.Second,
 			},
 		},
+		workerPool: semaphore.NewWeighted(int64(maxWorkers)),
 	}
 }
 
 func (y *YahooFinanceClient) GetQuoteHistory(ctx context.Context, symbol string, days int) ([]*YFQuote, error) {
-	cookies := map[string]string{
+	if err := y.workerPool.Acquire(ctx, 1); err != nil {
+		return nil, fmt.Errorf(
+			"yahoo_finance_client::GetQuoteHistory: failed to aquire semaphore: %v",
+			err,
+		)
 	}
+	defer y.workerPool.Release(1)
+
 	timeEnd := time.Now()
 	timeStart := timeEnd.AddDate(0, 0, -1*days)
 
 	// Params: symbol, start timestamp, end timestamp
 	const urlFormat = "https://query1.finance.yahoo.com/v7/finance/download/%s?period1=%d&period2=%d&interval=1d&events=history&crumb=pRB6UiIiFnn"
-
-	req, err := http.NewRequestWithContext(ctx, "GET", fmt.Sprintf(urlFormat, symbol, timeStart.Unix(), timeEnd.Unix()), nil)
+	reqUrl := fmt.Sprintf(urlFormat, symbol, timeStart.Unix(), timeEnd.Unix())
+	req, err := http.NewRequestWithContext(ctx, "GET", reqUrl, nil)
 	if err != nil {
 		return nil, err
-	}
-
-	for key, val := range cookies {
-		req.AddCookie(&http.Cookie{
-			Name:  key,
-			Value: val,
-		})
 	}
 
 	resp, err := y.client.Do(req)
 	if err != nil {
-		fmt.Printf("http error: %v", err)
 		return nil, err
 	}
 
-	fmt.Printf("Status: %v", resp.Status)
+	if resp.Status != "200 OK" {
+		return nil, fmt.Errorf(
+			`yahoo_finance_client::GetQuoteHistory: requested failed with status=%v, url="%v"`,
+			resp.Status,
+			reqUrl,
+		)
+	}
 
 	defer resp.Body.Close()
 	reader := csv.NewReader(resp.Body)
